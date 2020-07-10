a0d87bd8511150a5e2d549daf3a81d8fab5d538d
Krystal Guzman <kryguzman@gmail.com>
2020-07-10 10:18:01 -0700

    adds comments to intersection

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -5,13 +5,23 @@ def intersection(arrays):
     cache = {}
     len = 0
     result = []
+    
+    # loops through arrays
     for array in arrays:
         len +=1
+
+        # loops through numbers in array
         for num in array:
+
+            #adds number to cache
             if num not in cache:
                 cache[num] = 1
+            #increases number in cache
             else:
                 cache[num] += 1
+
+                #if number is in the array and not in results
+                #adds to results
                 if cache[num] == len and num not in result:
                     result.append(num)
 
