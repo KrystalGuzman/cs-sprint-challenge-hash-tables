fde49ccdf62cb25717ed575c12b1cccb155b3b56
Krystal Guzman <kryguzman@gmail.com>
2020-07-10 09:34:15 -0700

    finishes negatives

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,16 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    result = []
+
+    #adds all number to cache
+    for num in a:
+        cache[num] = 1
+        #if current's inverse is in cache,
+        # add positive to list, no 0
+        if num != 0 and -num in cache:
+            result.append(abs(num))
 
     return result
 
